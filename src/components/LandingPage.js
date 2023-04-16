fa56ede26bb8b4cf6f6fa8ecbe131b44ce69cb2c
20a91a0578 <20a91a0578@aec.edu.in>
2023-04-16 12:28:37 +0530

    changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
-import Img1 from './image1.png'
-import Img2 from './image2.png'
-import Img3 from './image3.jpg'
-import Img4 from './image4.webp'
+import pic1 from './image1.png'
+import pic2 from './image2.png'
+import pic3 from './image3.jpg'
+import pic4 from './image4.webp'
 import { Link } from 'react-router-dom'
 function LandingPage() {
-  const imgCon = [Img1,Img2,Img3,Img4]
+  const imgCon = [pic1,pic2,pic3,pic4]
     const prev = '<'
     const next = '>'
-    let [ind,upInd] = useState(0)
+    let [index,setindex] = useState(0)
 
 
     
     useEffect(() => {
         const imageChange = setInterval(() => {
-            ind++;
-            if(ind > 3)
-                ind = 0;
-          upInd(ind);
+           index++;
+            if(index > 3)
+               index = 0;
+          setindex(index);
         }, 5000);
         return () => clearInterval(imageChange);
       }, []);
@@ -26,23 +26,23 @@ function LandingPage() {
   return (
     <>
     <br/><br/>
-        <div className='carouselClass'>
+        <div className='classcarousel'>
         <div className='prev arrow' onClick={()=>{
-            ind--;
-            if(ind < 0)
-                ind = 3;
-            upInd(ind);
+           index--;
+            if(index < 0)
+               index = 3;
+            setindex(index);
         }}> <p> {prev} </p>  </div>
         <div className='next arrow'onClick={()=>{
-            ind++;
-            if(ind > 3)
-                ind = 0;
-            upInd(ind);
+           index++;
+            if(index > 3)
+               index = 0;
+            setindex(index);
         }}> <p> {next} </p> </div>
-        <img src={imgCon[ind]} alt=""/>
+        <img src={imgCon[index]} alt=""/>
         </div>
         <br/>
-        <button className='createCV' ><Link to ='/create-resume'>CreateResume</Link></button>
+        <button className='createresume' ><Link to ='/create-resume'>CreateResume</Link></button>
         </>
   )
 }
